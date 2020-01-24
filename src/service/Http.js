1748770f84d9e4ce1af92943e4abdf06f73d5f3f
sergio salomao <sergio.salomao@fabricainfo.com>
2020-01-24 18:45:14 -0300

    ajuste

--- src/service/Http.js
+++ src/service/Http.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 export const http = axios.create({
   // baseURL: `${process.env.VUE_APP_HOST}`
-  // baseURL: 'http://191.190.100.27:9005/api/',
+  baseURL: 'http://191.190.100.27:9005/api/',
   // baseURL: 'http://192.168.0.100:9005/api/',
-  baseURL: 'http://192.168.1.224:8000/api/',
+  // baseURL: 'http://192.168.1.224:8000/api/',
 });
 
 // http.interceptors.request.use((config) => {
