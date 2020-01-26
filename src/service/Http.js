88b5b0e1f954903aae267c832ec6597a34b51a25
Sergio Salomao <salomao.leite@hotmail.com>
2020-01-25 22:05:21 -0300

    resolvendo conflitos

--- src/service/Http.js
+++ src/service/Http.js
@@ -1,11 +1,7 @@
 import axios from 'axios';
 
 export const http = axios.create({
-<<<<<<< HEAD
   baseURL: 'http://192.168.0.100:9005/api/',
-=======
-  baseURL: 'http://191.190.100.27:9005/api/',
->>>>>>> a87e407ee62f97633648ae5d4dba50054a93fd94
 });
 
 http.interceptors.request.use((config) => {
